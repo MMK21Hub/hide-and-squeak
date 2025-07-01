408940c9dc567e78eaf3d7bdb3af2b5a31ddaed1
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-01 23:55:49 +0100

    Add a joinGame API procedure

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,19 +2,48 @@ import { z } from "zod"
 import { publicProcedure, router } from "./trpc"
 import { createHTTPServer } from "@trpc/server/adapters/standalone"
 import cors from "cors"
+import { PrismaClient } from "../generated/prisma"
+import { TRPCError } from "@trpc/server"
 
-const validLongitude = z.number().gte(-180).lte(180)
-const validLatitude = z.number().gte(-90).lte(90)
+const prisma = new PrismaClient()
 
 const appRouter = router({
+  /** Returns any sort of "pong" response that demonstrates the API is functioning. */
   ping: publicProcedure.query(() => {
     return `Pong! The time is ${new Date().toISOString()}`
   }),
-  putCoordinates: publicProcedure
-    .input(z.tuple([validLatitude, validLongitude]))
+  joinGame: publicProcedure
+    .input(
+      z.object({
+        code: z.string().min(1).max(256),
+        username: z.string().min(1).max(128),
+      })
+    )
     .mutation(async (opts) => {
       const { input } = opts
-      return `Coordinates are (${input[0]}, ${input[1]})`
+      const matchedGame = await prisma.game.findUnique({
+        where: { code: input.code },
+      })
+      if (!matchedGame)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `Game with code ${input.code} not found`,
+        })
+      if (!matchedGame.active)
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: `Game is no longer active`,
+        })
+      const newPlayer = await prisma.player.create({
+        data: {
+          name: input.username,
+          gameId: matchedGame.id,
+        },
+      })
+      return {
+        player: newPlayer,
+        game: matchedGame,
+      }
     }),
 })
 
@@ -24,7 +53,14 @@ const server = createHTTPServer({
 })
 
 const PORT = 3010
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+
+try {
+  server.listen(PORT)
+  console.log(`Server listening on port ${PORT}`)
+} catch (error) {
+  console.error(error)
+} finally {
+  prisma.$disconnect()
+}
 
 export type AppRouter = typeof appRouter
