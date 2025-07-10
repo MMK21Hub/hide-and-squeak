61d3f31c8811ffe9b2d6fa2605f4af6a09ccbdec
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-10 16:35:00 +0100

    Display the bounds on the map screen

--- frontend/src/MapScreen.tsx
+++ frontend/src/MapScreen.tsx
@@ -1,14 +1,27 @@
+import L from "leaflet"
 import { Game } from "hide-and-squeak-server"
 import LeafletMap from "./LeafletMap"
 import { OSMFTileServerLayer } from "./mapLayers"
 
 function MapScreen({ game }: { game: Game }): JSX.Element {
+  const gameBounds = L.geoJSON(game.gameBounds, {
+    style: {
+      color: "var(--color-error)",
+      weight: 2,
+    },
+  })
+
   return (
     <>
       <LeafletMap
         onMount={(map) => {
-          map.setView([51, 0], 10)
           OSMFTileServerLayer().addTo(map)
+          console.debug(gameBounds, "gameBounds")
+          gameBounds.addTo(map)
+          map.fitBounds(gameBounds.getBounds(), {
+            padding: [20, 20],
+            maxZoom: 18,
+          })
         }}
         class="h-full w-full"
       />
