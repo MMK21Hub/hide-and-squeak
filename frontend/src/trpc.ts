cd30c4ecd53ac73fa4f793d1e72ec0e61feb0076
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-19 18:07:42 +0100

    Remove error that will never happen

--- frontend/src/trpc.ts
+++ frontend/src/trpc.ts
@@ -2,14 +2,8 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client"
 import type { AppRouter } from "hide-and-squeak-server"
 
 const API_URL_ENV = "HIDE_AND_SQUEAK_API"
-const apiURL: string | null =
+const apiURL: string =
   localStorage.getItem("api_url") || import.meta.env[API_URL_ENV] || "/trpc"
-
-if (!apiURL) {
-  throw new Error(
-    `No API URL available. Ensure the ${API_URL_ENV} environment variable was correctly set when the frontend was built. See deployment instructions for details.`
-  )
-}
 
 export const trpc = createTRPCClient<AppRouter>({
   links: [
