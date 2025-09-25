bab1eda51a0ee2794e8d02ca1c0eb7cf429ba8e8
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-25 23:51:40 +0100

    Improve text on current game screen

--- frontend/src/CurrentGameScreen.tsx
+++ frontend/src/CurrentGameScreen.tsx
@@ -4,11 +4,12 @@ function CurrentGameScreen({ game }: { game: Game }): JSX.Element {
   return (
     <div class="flex flex-col gap-2 h-full m-4">
       <h1 class="text-3xl mb-2">
-        Current game: <strong>{() => game.name}</strong>
+        You have joined <strong>{() => game.name}</strong>
       </h1>
-      <p>
+      <p class="mb-6">
         Note: Most of this screen's content is a placeholder, and will be
-        improved in the future. Enjoy the debug info!
+        improved in the future. Enjoy the debug info, and check out the map view
+        too!
       </p>
       <p class="">
         Game code: <strong>{game.code.toUpperCase()}</strong>
