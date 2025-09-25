86458360319d769b947cdba2003bd3c6bf626cbc
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-25 23:29:07 +0100

    Show current game in top app bar

--- frontend/src/TopAppBar.tsx
+++ frontend/src/TopAppBar.tsx
@@ -1,10 +1,16 @@
+import { appState } from "./globalState"
+
 function TopAppBar() {
   const APP_NAME = "Hide and Squeak"
 
   return (
-    <header class="navbar bg-base-100 shadow-sm">
+    <header class="navbar bg-base-100 shadow-sm space-x-6">
       <h1 class="sr-only">{APP_NAME}</h1>
       <a class="btn btn-ghost text-xl">{APP_NAME}</a>
+      <div>
+        Current game:{" "}
+        <span class="font-bold">{appState.currentGame?.name}</span>
+      </div>
     </header>
   )
 }
