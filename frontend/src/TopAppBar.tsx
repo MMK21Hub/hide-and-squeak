2c7a5bd53d6d2dd4573a9e40469e3e802c1d5a86
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-25 23:58:37 +0100

    Only show game in top app bar when a game is active

--- frontend/src/TopAppBar.tsx
+++ frontend/src/TopAppBar.tsx
@@ -7,10 +7,15 @@ function TopAppBar() {
     <header class="navbar bg-base-100 shadow-sm space-x-6">
       <h1 class="sr-only">{APP_NAME}</h1>
       <a class="btn btn-ghost text-xl">{APP_NAME}</a>
-      <div>
-        Current game:{" "}
-        <span class="font-bold">{appState.currentGame?.name}</span>
-      </div>
+      {() => {
+        const game = appState.currentGame
+        if (!game) return
+        return (
+          <div>
+            Current game: <span class="font-bold">{game.name}</span>
+          </div>
+        )
+      }}
     </header>
   )
 }
